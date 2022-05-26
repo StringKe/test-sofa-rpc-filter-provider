cf279f5609d511faa5e1ca6272684393a1233d6e
陈泉 <authemail@qq.com>
2022-05-26 10:59:46 +0800

    feat: init

--- test-filter/src/main/java/com/test/rpc/filter/SaTokenSofaRpcConsumerFilter.java
+++ test-filter/src/main/java/com/test/rpc/filter/SaTokenSofaRpcConsumerFilter.java
@@ -1,6 +1,8 @@
 package com.test.rpc.filter;
 
 
+import cn.dev33.satoken.stp.StpUtil;
+import cn.dev33.satoken.util.SaTokenConsts;
 import com.alipay.sofa.rpc.context.RpcInvokeContext;
 import com.alipay.sofa.rpc.core.exception.SofaRpcException;
 import com.alipay.sofa.rpc.core.request.SofaRequest;
@@ -32,6 +34,9 @@ public class SaTokenSofaRpcConsumerFilter extends Filter {
         log.info("consumer invoke after response = " + RpcInvokeContext.getContext().getAllResponseBaggage());
 
         SofaResponse response = invoker.invoke(request);
+
+        // 当子服务更新了Sa-Token，则更新父服务的 Sa-Token 目前无法及时获取到 ResponseBaggage
+        // StpUtil.setTokenValue(RpcInvokeContext.getContext().getRequestBaggage(SaTokenConsts.JUST_CREATED_NOT_PREFIX));
 
         log.info("consumer invoke before request = " + RpcInvokeContext.getContext().getAllRequestBaggage());
         log.info("consumer invoke before response = " + RpcInvokeContext.getContext().getAllResponseBaggage());
